de2c16d609bebb60f41a90aafcdeb66a77d9b1ef
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 05:54:31 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+   This class demonstrates the Hanoi class, which
+   displays the steps necessary to solve the Towers
+   of Hanoi game.
+*/
+
+public class Main
+{
+   static public void main(String[] args)
+   {
+      Hanoi towersOfHanoi = new Hanoi(9);
+   }
+}
